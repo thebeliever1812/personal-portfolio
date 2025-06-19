c4fdb55e7f3b8c0dee670c830ca1e1690dd13b6c
Basir Ahmad <basirahmadmalik@gmail.com>
2025-06-19 18:51:17 +0530

    Add animation to profile image in HeroSection for enhanced visual appeal

--- app/components/HomePageSections/HeroSection.tsx
+++ app/components/HomePageSections/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ResumeButton } from "../index";
 import { Libre_Baskerville, Rubik } from 'next/font/google'
 import Image from 'next/image';
+import styles from './HeroSection.module.css'
 
 const heroFont = Libre_Baskerville({
     weight: ['400', '700'],
@@ -18,7 +19,7 @@ const rubik = Rubik({
 const HeroSection: React.FC = () => {
     return (
         <section className="hero-section w-full min-h-screen px-4 flex flex-col md:flex-row-reverse gap-5 lg:gap-10 justify-center md:justify-center items-center">
-            <div className='profile-image w-full max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg aspect-square relative'>
+            <div className={`profile-image w-full max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg aspect-square relative ${styles['profile-up-down']}`}>
                 <Image
                     src="/profile_picture.png"
                     alt="Basir Ahmad smiling in a professional headshot, wearing a neutral shirt, set against a simple background. The mood is friendly and approachable. No visible text in the image."
