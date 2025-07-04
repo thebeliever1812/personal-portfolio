b720517a602fa39393d714ad14d0b339169a7697
Basir Ahmad <basirahmadmalik@gmail.com>
2025-07-04 15:32:29 +0530

    Enhance fadeIn function to support animation type; update ProjectSpotlight component for improved animation effects

--- app/variants/variants.ts
+++ app/variants/variants.ts
@@ -1,8 +1,12 @@
-const fadeIn = (direction: string = "", delay: number = 0) => {
+const fadeIn = (
+	direction: string = "",
+	delay: number = 0,
+	type: "tween" | "spring" = "tween"
+) => {
 	return {
 		hidden: {
 			opacity: 0,
-			y: direction === "down" ? 20 : 0,
+			y: direction === "up" ? -20 : direction === "down" ? 20 : 0,
 			x: direction === "left" ? -40 : direction === "right" ? 40 : 0,
 		},
 		show: {
@@ -13,7 +17,7 @@ const fadeIn = (direction: string = "", delay: number = 0) => {
 				duration: 0.5,
 				delay: delay,
 				ease: "easeInOut" as const,
-				type: "tween" as const,
+				type: type,
 			},
 		},
 		view: {
